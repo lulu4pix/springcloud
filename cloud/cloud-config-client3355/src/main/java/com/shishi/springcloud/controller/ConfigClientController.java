c208494281620063d3bd49e09be6bc12fc3a5c68
lulu4pix <914104723@qq.com>
2021-05-28 15:11:41 +0800

    2021/5/28 cloud-config-client refresh

--- cloud/cloud-config-client3355/src/main/java/com/shishi/springcloud/controller/ConfigClientController.java
+++ cloud/cloud-config-client3355/src/main/java/com/shishi/springcloud/controller/ConfigClientController.java
@@ -2,16 +2,18 @@ package com.shishi.springcloud.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
     private String configInfo;
 
-    @GetMapping("/configInfo")
+    @PostMapping("/configInfo")
     public String getConfigInfo() {
         return configInfo;
     }
