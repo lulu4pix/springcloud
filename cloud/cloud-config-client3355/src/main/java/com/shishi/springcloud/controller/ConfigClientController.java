9410453f934e4a91f14bb793b2a56add0581f551
lulu4pix <914104723@qq.com>
2021-05-28 18:21:49 +0800

    2021/5/28 cloud-config-client auto refresh fix bug

--- cloud/cloud-config-client3355/src/main/java/com/shishi/springcloud/controller/ConfigClientController.java
+++ cloud/cloud-config-client3355/src/main/java/com/shishi/springcloud/controller/ConfigClientController.java
@@ -3,7 +3,9 @@ package com.shishi.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +15,7 @@ public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
 
-    @PostMapping("/configInfo")
+    @RequestMapping("/configInfo")
     public String getConfigInfo() {
         return configInfo;
     }
