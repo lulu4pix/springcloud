0e4d8a847c25ef527845241db2f0e70f44945c6e
lulu4pix <914104723@qq.com>
2021-05-31 09:39:04 +0800

    2021/5/30 cloudalibaba nacos config

---
+++ cloud/cloudalibaba-nacos-config-client4455/src/main/java/com/shishi/springcloudalibaba/NacosConfigMain4455.java
@@ -0,0 +1,11 @@
+package com.shishi.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigMain4455 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigMain4455.class, args);
+    }
+}
